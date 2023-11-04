127711c29d730e288a6e2cc95d77d2c330006ca8
sandermeland <sandermeland1@gmail.com>
2023-11-04 12:05:24 +0100

    Initial commit

---
+++ code_map/new_meters.py
@@ -0,0 +1,82 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import datetime
+import random as rand
+from code_map import Inputs
+
+
+class PowerMeter:
+    def __init__(self, meter_id, response_time, up_flex_volume, down_flex_volume, direction, sleep_time, consumption_data, area):
+        self.response_time = response_time
+        self.direction = direction
+        self.sleep_time = sleep_time
+        self.consumption_data = consumption_data
+        self.meter_id = meter_id
+        self.area = area
+        self.up_flex_volume = up_flex_volume
+        self.down_flex_volume = down_flex_volume
+        
+
+
+
+def preprocess_df(df, version_variables : Inputs.GlobalVariables):
+    start_date = pd.Timestamp(version_variables.year, version_variables.start_month, version_variables.start_day, version_variables.start_hour).tz_localize('Europe/Oslo')
+    end_date = pd.Timestamp(version_variables.year, version_variables.end_month, version_variables.end_day, version_variables.end_hour).tz_localize('Europe/Oslo')
+    df["start_time_local"] = pd.to_datetime(df["start_time_local"])
+    #print(df["start_time_local"])
+    df["start_time_local"] = df["start_time_local"].dt.tz_convert('Europe/Oslo')
+    #print(df["start_time_local"])
+    df = df.loc[(df["start_time_local"] >= start_date) & (df["start_time_local"] <= end_date)]
+   # print(df["start_time_local"])
+    hours_in_timehorizone = (end_date - start_date).days*24 + (end_date - start_date).seconds/3600
+    #print(hours_in_timehorizone)
+    # for eac metering_point_id, check if there is data for the whole timehorizone
+    count_hours_df = df.groupby("metering_point_id")["value"].agg(["count"])
+   # print(count_hours_df)
+    missing_hours_df = count_hours_df.loc[(count_hours_df["count"] < hours_in_timehorizone)]
+    new_meter_ids = missing_hours_df.index.tolist()
+    df = df[(~df["metering_point_id"].isin(new_meter_ids))]
+    #print(df)
+    
+    df.rename(columns={'start_time_local':'Time(Local)'}, inplace=True)
+    #df.drop(columns = ["end_time_local"], inplace = True)
+    df["value"] = df["value"] * 0.001 # convert from KWh to MWh
+    return df
+
+
+
+
+def create_meter_objects(consumption_data : pd.DataFrame ,tf : Inputs.GlobalVariables ):
+    power_meters = {}
+    updated_df = preprocess_df(consumption_data, tf)
+
+    for counter, meter_id in enumerate(updated_df["metering_point_id"].unique()):
+        meter_values = updated_df.loc[(updated_df["metering_point_id"] == meter_id)]
+        meter_values = meter_values.drop(columns = "metering_point_id")
+        #flex_volume = meter_values.copy()
+        #flex_volume["value"] = flex_volume["value"]*0.5
+        
+        directions = ["up", "down", "both"]
+        direction_index = rand.randint(0,2) if counter % 5 == 0 else 2
+        area = updated_df['area'].loc[(updated_df["metering_point_id"] == meter_id)].iloc[0]
+        # response time = mean of the flex_volume divided by the max consumption in the dataset for that meter times a value
+        if direction_index == 0: # up
+            up_flex_volume = meter_values.copy()
+            up_flex_volume["value"] =  up_flex_volume["value"] - min(up_flex_volume["value"])
+            meter = PowerMeter(meter_id = meter_id, response_time = rand.random()*300, up_flex_volume= up_flex_volume , down_flex_volume = [], direction = directions[direction_index], sleep_time = rand.random()*30, consumption_data = meter_values, area = area )
+        elif direction_index == 1:
+            down_flex_volume = meter_values.copy()
+            down_flex_volume["value"] = max(down_flex_volume["value"]) - down_flex_volume["value"] 
+            meter = PowerMeter(meter_id = meter_id, response_time = rand.random()*300, up_flex_volume= [] , down_flex_volume = down_flex_volume, direction = directions[direction_index], sleep_time = rand.random()*30, consumption_data = meter_values, area = area )
+        else:
+            up_flex_volume = meter_values.copy()
+            up_flex_volume["value"] = up_flex_volume["value"] - min(up_flex_volume["value"])
+
+            down_flex_volume = meter_values.copy()
+            down_flex_volume["value"] = max(down_flex_volume["value"]) - down_flex_volume["value"] 
+            meter = PowerMeter(meter_id = meter_id, response_time = rand.random()*300, up_flex_volume= up_flex_volume , down_flex_volume = down_flex_volume, direction = directions[direction_index], sleep_time = rand.random()*30, consumption_data = meter_values, area = area )
+
+        power_meters[meter_id] = meter
+    return power_meters
+
