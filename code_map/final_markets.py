4e58d13b7786b26690ca40b94a3f5bdee53d7e65
sandermeland <sandermeland1@gmail.com>
2023-11-30 08:37:24 +0100

    change structure to plot

--- code_map/final_markets.py
+++ code_map/final_markets.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import calendar 
 from datetime import datetime
 import pytz
--- code_map/new_meters.py
+++ code_map/new_meters.py
@@ -1,7 +1,6 @@
 from dataclasses import dataclass
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import datetime
 import random as rand
 from code_map import Inputs
