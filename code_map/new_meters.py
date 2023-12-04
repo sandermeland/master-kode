dd00c90ab659533f7ee22aaa870e81632f682aa2
sandermeland <sandermeland1@gmail.com>
2023-12-04 11:56:34 +0100

    update create_meter_objects function

--- code_map/new_meters.py
+++ code_map/new_meters.py
@@ -62,93 +62,6 @@ def combine_category_dfs(list_of_paths : list):
         dfs.append(df)
     return pd.concat(dfs, ignore_index = True)
         
-
-
-# SØO - does it make sense to have have down flex volume as there only are consumption meters?
-def old_create_meter_objects(consumption_data : pd.DataFrame ,tf : timeframes.TimeFrame, reference_tf : timeframes.TimeFrame, category_path_list : list ):
-    """
-    Creates the meter objects from the consumption data. 
-    The flex volume is calculated as the difference between the min/max value for the same hour and day of the week in the reference timeframe and the consumption data for the timeframe.
-    The response time is a random number between 0 and 300 seconds.
-    The sleep time is a random number between 0 and 30 minutes.
-    The direction is either up, down or both. 80 percent are chosen to be both while the last 20 percent are randomly chosen to be either up, both or down.
-    
-    Args:
-        consumption_data (pd.DataFrame): The consumption data
-        tf (timeframes.TimeFrame): The wanted timeframe
-        reference_tf (timeframes.TimeFrame): The reference timeframe for the flex volume to find min/max values
-        
-    Returns:
-        dict: a dictionary of the power meters
-    """
-    power_meters = {}
-    updated_df = preprocess_consumption_df(consumption_data, tf).copy()
-    monthly_df = preprocess_consumption_df(consumption_data, reference_tf).copy()
-    monthly_df['day_of_week'] = monthly_df['Time(Local)'].dt.day_name().astype('category')
-    monthly_df['hour'] = monthly_df['Time(Local)'].dt.hour
-
-
-    # Compute min and max values
-    grouped = monthly_df.groupby(['metering_point_id', 'day_of_week', 'hour'], observed=True)
-    aggregates = grouped['value'].agg(['min', 'max']).reset_index()
-
-    # Create dictionary for fast lookup
-    lookup_dict = {(row['metering_point_id'], row['day_of_week'], row['hour']): (row['min'], row['max']) for index, row in aggregates.iterrows()}
-
-    category_df = combine_category_dfs(category_path_list)
-
-    for counter, meter_id in enumerate(updated_df["metering_point_id"].unique()):
-        # the meter_id has to have values for all hours both in the timeframe and the reference timeframe
-        if meter_id in monthly_df["metering_point_id"].unique():
-            
-            meter_values = updated_df.loc[(updated_df["metering_point_id"] == meter_id)]
-            meter_values = meter_values.drop(columns = "metering_point_id")
-            
-            # calculate response time based on the categories
-            # response time = mean of the flex_volume divided by the max consumption in the dataset for that meter times a value
-
-            if meter_id in category_df["Identification"].unique():
-                if category_df["category"].loc[category_df["Identification"] == meter_id].iloc[0] == "ev_meters":
-                    response_time = 1
-                elif category_df["category"].loc[category_df["Identification"] == meter_id].iloc[0] == "harktech_meters":
-                    response_time = 5
-                else:
-                    response_time = rand.random()*300
-            else:
-                response_time = rand.random()*300
-            # Set direction
-            directions = ["up", "down", "both"]
-            direction_index = rand.randint(0,2) if counter % 5 == 0 else 2
-            # Set area
-            area = updated_df['area'].loc[(updated_df["metering_point_id"] == meter_id)].iloc[0]
-            
-            if direction_index == 0: # up
-                up_flex_volume = meter_values.copy()
-                
-                for hour in up_flex_volume["Time(Local)"]:
-                    up_flex_volume["value"].loc[up_flex_volume["Time(Local)"] == hour] =  up_flex_volume["value"].loc[up_flex_volume["Time(Local)"] == hour] - lookup_dict[(meter_id, hour.strftime('%A'), hour.hour)][0]
-                
-                meter = PowerMeter(meter_id = meter_id, response_time = response_time, up_flex_volume= up_flex_volume , down_flex_volume = [], direction = directions[direction_index], sleep_time = rand.random()*30, consumption_data = meter_values, area = area )
-            
-            elif direction_index == 1:
-                down_flex_volume = meter_values.copy()
-                for hour in down_flex_volume["Time(Local)"]:
-                    down_flex_volume["value"].loc[down_flex_volume["Time(Local)"] == hour] =  lookup_dict[(meter_id, hour.strftime('%A'), hour.hour)][1] - down_flex_volume["value"].loc[down_flex_volume["Time(Local)"] == hour]
-                meter = PowerMeter(meter_id = meter_id, response_time = response_time, up_flex_volume= [] , down_flex_volume = down_flex_volume, direction = directions[direction_index], sleep_time = rand.random()*30, consumption_data = meter_values, area = area )
-            else:
-                up_flex_volume = meter_values.copy()
-                down_flex_volume = meter_values.copy()
-                for hour in up_flex_volume["Time(Local)"]:
-                    up_flex_volume["value"].loc[up_flex_volume["Time(Local)"] == hour] =  up_flex_volume["value"].loc[up_flex_volume["Time(Local)"] == hour] - lookup_dict[(meter_id, hour.strftime('%A'), hour.hour)][0]
-                    down_flex_volume["value"].loc[down_flex_volume["Time(Local)"] == hour] =  lookup_dict[(meter_id, hour.strftime('%A'), hour.hour)][1] - down_flex_volume["value"].loc[down_flex_volume["Time(Local)"] == hour]
-            
-                meter = PowerMeter(meter_id = meter_id, response_time = response_time, up_flex_volume= up_flex_volume , down_flex_volume = down_flex_volume, direction = directions[direction_index], sleep_time = rand.random()*30, consumption_data = meter_values, area = area )
-
-            power_meters[meter_id] = meter
-        else:
-            continue
-    return power_meters
-
 def create_meter_objects(consumption_data : pd.DataFrame ,tf : timeframes.TimeFrame, reference_tf : timeframes.TimeFrame, category_path_list : list ):
     """
     Creates the meter objects from the consumption data. 
@@ -255,23 +168,7 @@ def create_meter_objects(consumption_data : pd.DataFrame ,tf : timeframes.TimeFr
             continue
     return power_meters
 
-consumption_data =pd.read_csv('../master-data/customers-data/added_type_and_comp.csv')
-tf = timeframes.one_day
-reference_tf = timeframes.one_month
-cat_path_list = ["../master-data/categorization_data/harktech_meters.csv",  "../master-data/categorization_data/ev_meters.csv"]
 
-    
-new_power_meter_dict = create_meter_objects(consumption_data = consumption_data, tf= tf, reference_tf= reference_tf, category_path_list=cat_path_list) 
-
-old_power_meter_dict = create_meter_objects(consumption_data = consumption_data, tf= tf, reference_tf= reference_tf, category_path_list=cat_path_list) 
-
-m_id =list(new_power_meter_dict.keys())[1]
-
-new_power_meter_dict[m_id].up_flex_volume.equals(old_power_meter_dict[m_id].up_flex_volume)
-
-new_power_meter_dict[m_id].up_flex_volume
-
-old_power_meter_dict[m_id].up_flex_volume
 
 """
 
